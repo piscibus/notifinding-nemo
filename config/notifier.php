692a438903782e0ecc7fccb83b541ec766d2504c
Eslam <eslamaboelwafa@gmail.com>
2020-07-06 04:04:39 +0200

    Modifying config file (remove sender id)

--- config/notifier.php
+++ config/notifier.php
@@ -2,7 +2,6 @@
 
 return [
     'firebase' => [
-        'key' => 'AAAAiE4K7m4:APA91bE_1JdLq8kDBcUOu62zeNtHWSZM4PssVgIZYc4UtmqQJ6pMFdVQqSnIs1FDsa0UkOJVRuDwCcQaGdnHbuFC5M-Nt2FanujDTwIhJEOjJyAOsb0ruBwlNvh8iZVSmnx1z23NiMUW',
-        'sender_id' => '585424891502',
+        'key' => env('FIREBASE_SERVER_KEY', ''),
     ],
 ];
